80288bcbea781f1620af6e24052767479b289386
Lucca Santos <lucasluccad.r@outlook.com>
2022-12-21 00:18:53 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+const PORT = 3000
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
